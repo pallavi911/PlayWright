d699e702c40ab4b5690e1422cff500be0ef14746
Pallavi Garg <pallavi.aspirefox@gmail.com>
2026-08-20 18:56:14 +0530

    updated hook

--- tests/hooks_demo.spec.js
+++ tests/hooks_demo.spec.js
@@ -11,21 +11,21 @@ test.beforeAll(async ({browser}) => {
 });
 
 // hook that is executed once per worker process after all tests.
-test.afterAll(async () => {
-  await context.tracing.stop({ path: 'hooks_trace.zip' });
-});
+// test.afterAll(async () => {
+//   await context.tracing.stop({ path: 'hooks_trace.zip' });
+// });
 
-test('Hello World',async({})=>{
-    await page.goto('https://playwright.dev/')
-    await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright')
-})
+// test('Hello World',async({})=>{
+//     await page.goto('https://playwright.dev/')
+//     await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright')
+// })
 
-test('get started link', async ({}) => {
-  await page.goto('https://playwright.dev/');
+// test('get started link', async ({}) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
